2f158fc5cb58a0dd6bb0fb7c3bd4abe29db4c112
micke754 <kevin.vncnt@gmail.com>
2025-01-26 22:00:42 +1300

    count_char() in progress

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+def count_words(text: str) -> int:
+    total_word_count: list[str] = []
+
+    for word in text.split():
+        total_word_count.append(word)
+
+    return len(total_word_count)
+
+
+def count_characters(text: str) -> dict[str, int]:
+    total_char_count: dict[str, int] = {}
+
+    for char in text.lower():
+        
+        
+        
+
+    return None
+
+
 def main():
     with open(file="./books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+        file_contents: str = f.read()
+        word_count: int = count_words(text=file_contents)
+    print(word_count)
 
 
 if __name__ == "__main__":
