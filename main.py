cedc64db39f1811c636ddef5fc0175969bcab831
micke754 <kevin.vncnt@gmail.com>
2025-01-28 17:41:52 +1300

    Print a Report Completed

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+
 def count_words(text: str) -> int:
     total_word_count: list[str] = []
 
@@ -8,22 +11,31 @@ def count_words(text: str) -> int:
 
 
 def count_characters(text: str) -> dict[str, int]:
-    total_char_count: dict[str, int] = {}
+    count: dict[str, int] = {}
 
     for char in text.lower():
-        
-        
-        
+        if char.isalpha():
+            if char not in count:
+                count[char] = 1
+            else:
+                count[char] = count[char] + 1
 
-    return None
-
-
-def main():
-    with open(file="./books/frankenstein.txt") as f:
-        file_contents: str = f.read()
-        word_count: int = count_words(text=file_contents)
-    print(word_count)
+    return count
 
 
-if __name__ == "__main__":
-    main()
+# Main execution
+
+with open(file="./books/frankenstein.txt") as f:
+    file_contents: str = f.read()
+    word_count: int = count_words(text=file_contents)
+    character_count: dict[str, int] = count_characters(text=file_contents)
+
+print("--- Begin report of books/frankenstein.txt ---")
+
+pprint(object=f"{word_count} words found in the document")
+print("\n")
+
+for letter, count in character_count.items():
+    pprint(object=f"The '{letter}' character was found {count} times")
+
+print("--- End report ---")
